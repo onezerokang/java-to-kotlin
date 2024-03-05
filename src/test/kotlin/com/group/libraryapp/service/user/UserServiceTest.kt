af6c5092253037516c283a2527acba8fe27eadf3
Wonyoung Kang <onezerokang@gmail.com>
2024-03-05 17:55:29 +0900

    Test: UserService 테스트

--- src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -1,8 +1,13 @@
 package com.group.libraryapp.service.user
 
+import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.groups.Tuple
+import org.assertj.core.groups.Tuple.tuple
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -13,6 +18,12 @@ class UserServiceTest @Autowired constructor(
         private val userRepository: UserRepository,
         private val userService: UserService
 ) {
+
+    @AfterEach
+    fun tearDown() {
+        userRepository.deleteAllInBatch()
+    }
+
     @DisplayName("회원을 등록할 수 있다.")
     @Test
     fun saveUser() {
@@ -27,5 +38,56 @@ class UserServiceTest @Autowired constructor(
         assertThat(users).hasSize(1)
         assertThat(users[0].name).isEqualTo("최태현")
         assertThat(users[0].age).isNull()
+    }
+
+    @DisplayName("등록된 회원을 조회할 수 있다.")
+    @Test
+    fun getUsers() {
+        // given
+        userRepository.saveAll(listOf(
+            User("A", 20),
+            User("B", null),
+        ))
+
+        // when
+        val users = userService.getUsers()
+
+        //then
+        assertThat(users).hasSize(2)
+            .extracting("name", "age")
+            .containsExactlyInAnyOrder(
+                tuple("A", 20),
+                tuple("B", null)
+            )
+    }
+
+    @DisplayName("회원의 이름을 변경할 수 있다.")
+    @Test
+    fun updateUserName() {
+        // given
+        val savedUser = userRepository.save(User("A", null))
+        val request = UserUpdateRequest(savedUser.id, "B")
+
+        // when
+        userService.updateUserName(request)
+
+        //then
+        val user = userRepository.findAll()[0]
+        assertThat(user.name).isEqualTo("B")
+    }
+
+    @DisplayName("회원 계정을 삭제할 수 있다.")
+    @Test
+    fun deleteUser() {
+        // given
+        val name = "A"
+        val savedUser = userRepository.save(User(name, null))
+
+        // when
+        userService.deleteUser(name)
+
+        //then
+        val users = userRepository.findAll()
+        assertThat(users).isEmpty()
     }
 }
